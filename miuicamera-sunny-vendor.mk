563aa211800bfafcc80cbc7226f44e9af370887c
Cyber Knight <cyberknight755@gmail.com>
2025-04-12 01:23:30 +0800

    MiuiCamera: Import from sunny V14.0.9.0.SKGMIXM
    - Track MiuiCamera.apk as LFS.
    
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

---
+++ miuicamera-sunny-vendor.mk
@@ -0,0 +1,11 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/xiaomi/miuicamera-sunny
+
+PRODUCT_PACKAGES += \
+    libcamera_algoup_jni.xiaomi \
+    libcamera_mianode_jni.xiaomi \
+    MiuiCamera
